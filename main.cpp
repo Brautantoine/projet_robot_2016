7a03af89d194921e02972cc3c8c36d6c55535ed8
Braut Antoine <antoine.braut@gmail.com>
2017-01-03 09:27:53 +0100

    Add files via upload
    version commenté et implemantation de l'automate de pre suivie (non testé)

--- main.cpp
+++ main.cpp
@@ -1,67 +1,40 @@
 #include "nboard.h"
 
-unsigned char state(1);
-unsigned char core(1);
-float speed(1);
+unsigned char state(1); //variable d etat de l automate ligne blanche
+unsigned char state_P(1); //variable d etat de lautomate de pre suivie
+unsigned char core(1);  //variable d arret pour l automate ligne blanche
+
+float seuil(0.45); //seuil capteur
+
 int i=0;
 
-PwmOut RWM1(PB_5); //droite
-PwmOut RWM2(PB_4); //gauche
+PwmOut PWMD(PB_5); //mouteur droite
+PwmOut PWMG(PB_4); //moteur gauche
 
-IHM ihm;
+IHM ihm; //instance de l ihm
 
-float lire_capteur(int n_capteur)
-{
-    BusMux = n_capteur;
-    return AnaIn.read();
-}
+float lire_capteur(int n_capteur);
+void tout_droit();
+void A_Gauche();
+void A_Droite();
+void Stop_Robot();
+void nouvel_etat_pre_suivie();
 
-void tout_droit()
-{
-    BusMux=7;
-    RWM1.write(AnaIn.read());
-    RWM2.write(AnaIn.read());
-    if((lire_capteur(1)<float(0.45)&&lire_capteur(2)>float(0.45)))state=GAUCHE;
-    if((lire_capteur(1)>float(0.45)&&lire_capteur(2)<float(0.45)))state=DROITE;
-    if(!BPP)state=STOP;
-    
-    
-}
-
-void babord_capitaine()
-{
-    BusMux=7;
-    RWM1.write(AnaIn.read());
-    RWM2.write(float(0.45)*float(AnaIn.read()));
-    if((lire_capteur(1)>float(0.45)&&lire_capteur(2)>float(0.45)))state=TOUT_DROIT;
-    if(!BPP)state=STOP;
-}
-
-void tribord_capitaine()
-{
-    BusMux=7;
-    RWM1.write(float(0.45)*float(AnaIn.read()));
-    RWM2.write(AnaIn.read());
-    if((lire_capteur(1)>float(0.45)&&lire_capteur(2)>float(0.45)))state=TOUT_DROIT;
-    if(!BPP)state=STOP;
-}
 
 int main() 
 {
     BusMux=7;
-    RWM1.period(0.00005);
-    RWM2.period(0.00005);
+		/*initialisation des moteur*/
+    PWMD.period(0.00005);
+    PWMG.period(0.00005);
     
-    while(1)
-    {
+    /* affichage de bienvenue*/
+		ihm.LCD_gotoxy(0,0);
+		ihm.LCD_printf("Gr A6 equpie BCDL");
+		wait(1);
+		
      while(jack) {
-                BusMux=7;
-                ihm.LCD_gotoxy(0,0);
-                ihm.LCD_printf("pot=%f",AnaIn.read());
-                BusLed=0X00;
-                for(i=0; i<6; i++) {
-                    if(lire_capteur(i)>float(0.5))BusLed[i]=1;
-                }
+						nouvel_etat_pre_suivie();
             }
             BusMux=7;
     while(core) 
@@ -78,17 +51,132 @@ int main()
                 tout_droit();
                 break;
             case GAUCHE:
-                babord_capitaine();
+                A_Gauche();
                 break;
             case DROITE:
-                tribord_capitaine();
+                A_Droite();
                 break;
             case STOP:
-                RWM1.write(0);
-                RWM2.write(0);
-                core=0;
+							Stop_Robot();
+							break;
          }       
     }
-    while(1);
+    while(1)
+    {
     }
+}
+
+float lire_capteur(int n_capteur) //fonctions de lecture des capteur (renvoie la valeur du capteur en entrée)
+{
+    BusMux = n_capteur;
+    return AnaIn.read();
+}
+
+void tout_droit() //procedure d actualisations du moteur (1:1)
+{
+    BusMux=7;
+    PWMD.write(AnaIn.read());
+    PWMG.write(AnaIn.read());
+    if((lire_capteur(2)<float(seuil)&&lire_capteur(3)>float(seuil)))state=GAUCHE;
+    else if((lire_capteur(2)>float(seuil)&&lire_capteur(3)<float(seuil)))state=DROITE;
+    if(!BPP)state=STOP;
+    
+    
+}
+
+void A_Gauche() //procedure d actualisations du moteur (0.4:1)
+{
+    BusMux=7;
+    PWMD.write(AnaIn.read());
+    PWMG.write(float(0.4)*float(AnaIn.read()));
+    if((lire_capteur(2)>float(seuil)&&lire_capteur(3)>float(seuil)))state=TOUT_DROIT;
+    if(!BPP)state=STOP;
+}
+
+void A_Droite() //procedure d actualisations du moteur (1:0.4)
+{
+    BusMux=7;
+    PWMD.write(float(0.4)*float(AnaIn.read()));
+    PWMG.write(AnaIn.read());
+    if((lire_capteur(2)>float(seuil)&&lire_capteur(3)>float(seuil)))state=TOUT_DROIT;
+    if(!BPP)state=STOP;
+}
+
+void Stop_Robot() //procedure d actualisations du moteur (0:0)
+{
+	PWMD.write(0);
+  PWMG.write(0);
+  core=0; //sortie de boucle while
+}
+void nouvel_etat_pre_suivie() //automate de pre suivie
+{
+	int val(0);
+	int val_2(0);
+	switch (state_P)
+	{
+		case WAIT:
+			/*affichage des capteur et du vpot en tps réel*/
+			BusMux=7;
+      ihm.LCD_gotoxy(0,0);
+      ihm.LCD_printf("pot=%f",AnaIn.read());
+      BusLed=0X00;
+      for(i=0; i<6; i++) {
+				if(lire_capteur(i)>float(0.5))BusLed[i]=1;
+      }
+			if(!BP0)state_P=CAPT;
+			else if(!BP1)state_P=MOT;
+			else if(!BP2)state_P=POL;
+			break;
+		case CAPT:
+			/*comme wait mais avec un calibrage possible*/
+			BusMux=7;
+      ihm.LCD_gotoxy(0,0);
+      ihm.LCD_printf("pot=%f att calibrage",AnaIn.read());
+		  if(!BP0)seuil=AnaIn.read();
+      BusLed=0X00;
+      for(i=0; i<6; i++) {
+				if(lire_capteur(i)>float(seuil))BusLed[i]=1;
+      }
+			if(!BPP)state_P=WAIT;
+			break;
+		case MOT:
+			BusMux = 7;
+           
+        // la fonction AnaIn.read lit le potention mettre, la variable val prend la valeur du potentiometre 
+        val=AnaIn.read();
+           
+        // affichage de la valeur du potentiometre
+        ihm.LCD_gotoxy(0,0);
+        ihm.LCD_printf("Vitesse_d=%f",val);
+        
+        // la variable val_2 prend la valeur (1-valeur_potentiometre)    
+        val_2=1-AnaIn.read();
+        
+        // affichage de la valeur du potentiometre    
+        ihm.LCD_gotoxy(1,0);
+        ihm.LCD_printf("Vitesse_g=%f",val_2);
+       
+          if(BP0==0)
+           {
+            // PWM0 prend la valeur de la variable val
+            PWMD.write(val);                       
+            
+            // PWM1 prend la valeur de la variable val_2
+            PWMG.write(val_2);
+           }
+					 wait(0.5);
+					 if(!BPP)state_P=WAIT;
+			break;
+		case POL:
+				/*execute un polygones puis attend un appui sur le bp3*/
+				PWMG.write(0.2);
+        PWMD.write(0.2);
+        wait(2.5);
+        PWMG.write(0.3);
+        PWMD.write(0);
+        wait(0.9);
+				while(BPP);
+				state_P=WAIT;
+			break;
+	}
 }
