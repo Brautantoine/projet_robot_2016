baaecdf04e2bbcf34353b816ebf613b91136619b
Braut Antoine <antoine.braut@gmail.com>
2017-01-03 09:29:40 +0100

    header
    le .h commenté et a jour

---
+++ nboard.h
@@ -0,0 +1,45 @@
+#include "mbed.h"
+#include <BusOut.h>
+#include <IHM.h>
+#include <PwmOut.h>
+
+
+
+/*DigitalOut Led0(PB_3);
+DigitalOut Led1(PA_7);
+DigitalOut Led2(PA_6);
+DigitalOut Led3(PA_5);*/
+BusOut BusLed(PB_3,PA_7,PA_6,PA_5,PA_3,PA_1,PA_0,PA_2); //bus de sortie des led
+    //LED 0
+DigitalIn BP0(PA_9, PullUp);
+DigitalIn BP1(PA_10, PullUp);
+DigitalIn BP2(PB_0, PullUp);
+DigitalIn BPP(PB_7, PullUp);
+DigitalIn jack(PB_6, PullUp);
+
+    //BP0
+    
+BusOut BusMux (PA_8,PF_1,PF_0); //bus de selection des entrees analogiques
+
+AnalogIn AnaIn(PB_1);
+    
+#define Ana0 000;
+#define Ana1 001;
+#define Ana2 010;
+#define Ana3 011;
+#define Ana4 100;
+#define Ana5 101;
+#define AIn 110;
+#define vpot 111;
+
+//symboliques des etat de l automate ligne blanche
+#define TOUT_DROIT 1
+#define GAUCHE 2
+#define DROITE 3
+#define STOP 4
+
+//symboliques des etat de l automate pre suivie
+#define WAIT 1
+#define CAPT 2
+#define MOT 3
+#define POL 4
